34713318ff054af6220965d965f488514c5b7a60
Daniel van Hoesel <daniel@danielvanhoesel.nl>
2016-02-26 11:56:33 +0100

    Add FBSnapshot tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,29 +1,87 @@
 import UIKit
 import XCTest
+import FBSnapshotTestCase
 import Dezignables
 
-class Tests: XCTestCase {
+class Tests: FBSnapshotTestCase {
+  
+  override func setUp() {
+    super.setUp()
+//    self.recordMode = true
+  }
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  func testDezignableView() {
+    let dezignableView = DezignableView(frame: CGRectMake(0, 0, 200, 200))
+    dezignableView.borderWidth = 5.0
+    dezignableView.borderTop = true
+    dezignableView.borderBottom = true
+    dezignableView.borderLeft = true
+    dezignableView.borderRight = true
+    dezignableView.cornerRadius = 20.0
+    dezignableView.borderColor = UIColor.blackColor()
+    dezignableView.backgroundColor = UIColor.redColor()
+    FBSnapshotVerifyView(dezignableView)
+  }
+  
+  func testScaleCorrection() {
+    let dezignableView = DezignableView(frame: CGRectMake(0, 0, 200, 200))
+    dezignableView.borderWidth = 1.0
+    dezignableView.borderTop = true
+    dezignableView.borderBottom = true
+    dezignableView.borderLeft = true
+    dezignableView.borderRight = true
+    dezignableView.cornerRadius = 20.0
+    dezignableView.borderColor = UIColor.blackColor()
+    dezignableView.backgroundColor = UIColor.redColor()
+    dezignableView.scaleCorrection = true
+    FBSnapshotVerifyView(dezignableView)
+  }
+  
+  func testTopBorder() {
+    let dezignableView = DezignableView(frame: CGRectMake(0, 0, 200, 200))
+    dezignableView.borderWidth = 5.0
+    dezignableView.borderTop = true
+    dezignableView.borderColor = UIColor.blackColor()
+    dezignableView.backgroundColor = UIColor.redColor()
+    FBSnapshotVerifyView(dezignableView)
+  }
+  
+  func testLeftBorder() {
+    let dezignableView = DezignableView(frame: CGRectMake(0, 0, 200, 200))
+    dezignableView.borderWidth = 5.0
+    dezignableView.borderLeft = true
+    dezignableView.borderColor = UIColor.blackColor()
+    dezignableView.backgroundColor = UIColor.redColor()
+    FBSnapshotVerifyView(dezignableView)
+  }
+  
+  func testRightBorder() {
+    let dezignableView = DezignableView(frame: CGRectMake(0, 0, 200, 200))
+    dezignableView.borderWidth = 5.0
+    dezignableView.borderRight = true
+    dezignableView.borderColor = UIColor.blackColor()
+    dezignableView.backgroundColor = UIColor.redColor()
+    FBSnapshotVerifyView(dezignableView)
+  }
+  
+  func testBottomBorder() {
+    let dezignableView = DezignableView(frame: CGRectMake(0, 0, 200, 200))
+    dezignableView.borderWidth = 5.0
+    dezignableView.borderBottom = true
+    dezignableView.borderColor = UIColor.blackColor()
+    dezignableView.backgroundColor = UIColor.redColor()
+    FBSnapshotVerifyView(dezignableView)
+  }
+  
+  func testBorderColor() {
+    let dezignableView = DezignableView(frame: CGRectMake(0, 0, 200, 200))
+    dezignableView.borderWidth = 5.0
+    dezignableView.borderTop = true
+    dezignableView.borderBottom = true
+    dezignableView.borderLeft = true
+    dezignableView.borderRight = true
+    dezignableView.borderColor = UIColor.blueColor()
+    dezignableView.backgroundColor = UIColor.redColor()
+    FBSnapshotVerifyView(dezignableView)
+  }
 }
